c0510037088a7cc641e7649f314d3c2ad83c05f0
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-01 17:29:02 +0200

    add validation

--- add.php
+++ add.php
@@ -5,11 +5,37 @@
 //   echo $_GET['title'];
 //   echo $_GET['ingredients'];
 // }
+
 if(isset($_POST['submit'])) {
-  echo $_POST['email'];
-  echo $_POST['title'];
-  echo $_POST['ingredients'];
+  // echo htmlspecialchars($_POST['email']);
+  // echo htmlspecialchars($_POST['title']);
+  // echo htmlspecialchars($_POST['ingredients']);
+  if(empty($_POST['email'])) {
+    echo 'An email is required <br>';
+  } else {
+    $email = $_POST['email'];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo 'email must be a valid email address';
+    }
+  }
+  if(empty($_POST['title'])) {
+    echo 'A title is required <br>';
+  } else {
+    $title = $_POST['title'];
+    if(preg_match('/^[a-zA-Z\s]+$/'), $title) {
+      echo 'Title must be letters and spaces only';
+    }
+  }
+  if(empty($_POST['ingredients'])) {
+    echo 'At lease one ingredient is required <br>';
+  } else {
+    $ingredients = $_POST['ingredients'];
+    if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+      echo 'Ingredients must be a comma separated list';
+    }
+  }
 }
+
 
 ?>
 
